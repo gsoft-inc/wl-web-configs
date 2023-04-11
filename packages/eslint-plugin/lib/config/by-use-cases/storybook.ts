2bfa26ea2109c8b12531079830da0c41cc36b448
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-04-11 15:20:58 -0400

    add mdx to configs

--- packages/eslint-plugin/lib/config/by-use-cases/storybook.ts
+++ packages/eslint-plugin/lib/config/by-use-cases/storybook.ts
@@ -1,6 +1,6 @@
 // https://github.com/storybookjs/eslint-plugin-storybook/blob/main/lib/configs/recommended.ts
 
-import { storybookFiles, mainStorybookFiles } from "../../utils/patterns";
+import { storybookFiles, mainStorybookFiles, mdxFiles } from "../../utils/patterns";
 import type { Linter } from "eslint";
 
 const config: Linter.Config = {
@@ -39,6 +39,10 @@ const config: Linter.Config = {
             rules: {
                 "storybook/no-uninstalled-addons": "warn"
             }
+        },
+        {
+            files: mdxFiles,
+            extends: ["plugin:mdx/recommended"]
         }
     ]
 };
