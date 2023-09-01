e6c57c8fd7fe612f958eee666559eb3eb22dc315
patricklafrance <pa.lafrance@gmail.com>
2023-09-01 17:27:29 -0400

    Fixing CI

--- sample/app/webpack.dev.js
+++ sample/app/webpack.dev.js
@@ -3,9 +3,8 @@
 import { defineDevConfig } from "@workleap/webpack-configs";
 import { swcConfig } from "./swc.dev.js";
 
-export default defineDevConfig({
+export default defineDevConfig(swcConfig, {
     fastRefresh: true,
-    swcConfig,
     environmentVariables: {
         "USE_MSW": process.env.USE_MSW
     }
