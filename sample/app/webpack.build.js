c9540f4158598f238cae1cf64fdac92ae297e791
Alexandre Beaudoin <alexandre.beaudoin@gsoft.com>
2023-09-05 18:06:55 -0400

    Rename PROFILE to WL_PROFILE and

--- sample/app/webpack.build.js
+++ sample/app/webpack.build.js
@@ -4,7 +4,7 @@ import { defineBuildConfig } from "@workleap/webpack-configs";
 import { swcConfig } from "./swc.build.js";
 
 export default defineBuildConfig(swcConfig, {
-    profile: process.env.PROFILE === "true",
+    profile: process.env.WL_PROFILE === "true",
     environmentVariables: {
         "USE_MSW": process.env.USE_MSW === "true"
     }
--- sample/app/webpack.dev.js
+++ sample/app/webpack.dev.js
@@ -4,7 +4,7 @@ import { defineDevConfig } from "@workleap/webpack-configs";
 import { swcConfig } from "./swc.dev.js";
 
 export default defineDevConfig(swcConfig, {
-    profile: process.env.PROFILE === "true",
+    profile: process.env.WL_PROFILE === "true",
     environmentVariables: {
         "USE_MSW": process.env.USE_MSW === "true",
         "STRING": "STRING_VALUE",
