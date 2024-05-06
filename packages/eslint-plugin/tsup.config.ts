8d2bcff4fe0241762457cc462f8c5c461673a2f8
Brian Sokol <t-brian.sokol@gsoft.com>
2024-05-06 12:21:10 -0500

    Removing unnecessary config values

--- packages/eslint-plugin/tsup.config.ts
+++ packages/eslint-plugin/tsup.config.ts
@@ -2,7 +2,5 @@ import { defineBuildConfig } from "@workleap/tsup-configs";
 
 export default defineBuildConfig({
     entry: ["./lib", "!./lib/plugins.d.ts"],
-    format: "esm",
-    target: "esnext",
     platform: "node"
 });
