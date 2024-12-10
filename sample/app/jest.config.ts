0509fa75e914261f2435236a56af63e647870af0
patricklafrance <pa.lafrance@gmail.com>
2024-12-10 17:25:54 -0500

    More stuff

--- sample/app/jest.config.ts
+++ sample/app/jest.config.ts
@@ -1,19 +1,19 @@
 import type { Config } from "jest";
-import { pathsToModuleNameMapper } from "ts-jest";
 import { swcConfig } from "./swc.jest.ts";
-import { compilerOptions } from "./tsconfig.json";
 
 const config: Config = {
+    testRegex: "/tests/*/.*\\.test\\.(ts|tsx)$",
+    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
     testEnvironment: "jsdom",
     transform: {
-        "^.+\\.(ts|tsx)$": ["@swc/jest", swcConfig as Record<string, unknown>]
+        "^.+\\.(js|ts|tsx)$": ["@swc/jest", swcConfig as Record<string, unknown>]
     },
     moduleNameMapper: {
-        "\\.css$": "identity-obj-proxy",
-        ...pathsToModuleNameMapper(compilerOptions.paths, {
-            prefix: "<rootDir>"
-        })
-    }
+        "\\.css$": "identity-obj-proxy"
+    },
+    cacheDirectory: "./node_modules/.cache/jest",
+    clearMocks: true,
+    verbose: true
 };
 
 export default config;
