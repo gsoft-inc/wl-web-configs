a510a6d23d3dfe8c8051a00807d53a092a6220e1
patricklafrance <pa.lafrance@gmail.com>
2024-12-10 16:50:46 -0500

    More stuff

--- packages/eslint-plugin/swc.jest.ts
+++ packages/eslint-plugin/swc.jest.ts
@@ -1,3 +1,4 @@
+// Importing directly from the package to prevent circular dependencies.
 import { defineJestConfig } from "../swc-configs/src/index.ts";
 
 export const swcConfig = defineJestConfig();
--- packages/eslint-plugin/tsup.config.ts
+++ packages/eslint-plugin/tsup.config.ts
@@ -1,3 +1,4 @@
+// Importing directly from the package to prevent circular dependencies.
 import { defineBuildConfig } from "../tsup-configs/src/index.ts";
 
 export default defineBuildConfig({
