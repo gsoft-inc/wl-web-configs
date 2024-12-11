9f3e01346a8ef8e9f091dc71fbfe95316af7cbdc
patricklafrance <pa.lafrance@gmail.com>
2024-12-11 14:25:34 -0500

    More stuff

--- packages/rsbuild-configs/src/storybook.ts
+++ packages/rsbuild-configs/src/storybook.ts
@@ -1,1 +1,86 @@
-// TBD
+import { defineConfig, type RsbuildConfig, type RsbuildPlugins, type SourceMap } from "@rsbuild/core";
+import { pluginReact, type PluginReactOptions } from "@rsbuild/plugin-react";
+import { pluginSvgr, type PluginSvgrOptions } from "@rsbuild/plugin-svgr";
+import { applyTransformers, type RsbuildConfigTransformer } from "./applyTransformers.ts";
+
+export type DefineStorybookDefineReactPluginConfigFunction = (defaultOptions: PluginReactOptions) => PluginReactOptions;
+export type DefineStorybookSvgrPluginConfigFunction = (defaultOptions: PluginSvgrOptions) => PluginSvgrOptions;
+
+export interface DefineStorybookConfigOptions {
+    plugins?: RsbuildPlugins;
+    sourceMap?: boolean | SourceMap;
+    react?: false | DefineStorybookDefineReactPluginConfigFunction;
+    svgr? : false | DefineStorybookSvgrPluginConfigFunction;
+    environmentVariables?: Record<string, unknown>;
+    transformers?: RsbuildConfigTransformer[];
+    verbose?: boolean;
+}
+
+function defaultDefineReactPluginConfig(options: PluginReactOptions) {
+    return options;
+}
+
+function defineSvgrPluginConfig(options: PluginSvgrOptions) {
+    return options;
+}
+
+export function defineStorybook(options: DefineStorybookConfigOptions = {}) {
+    const {
+        plugins = [],
+        sourceMap = {
+            js: "cheap-module-source-map",
+            css: true
+        },
+        react = defaultDefineReactPluginConfig,
+        svgr = defineSvgrPluginConfig,
+        // Using an empty object literal as the default value to ensure
+        // "process.env" is always available.
+        environmentVariables = {},
+        transformers = [],
+        verbose = false
+    } = options;
+
+    const config: RsbuildConfig = {
+        source: {
+            // Stringify the environment variables because the plugin does a direct text replacement. Otherwise, "production" would become production
+            // after replacement and cause an undefined var error because the production var doesn't exist.
+            // For more information, view: https://rsbuild.dev/guide/advanced/env-vars#using-define.
+            define: {
+                "process.env": Object.keys(environmentVariables).reduce((acc, key) => {
+                    acc[key] = JSON.stringify(environmentVariables[key]);
+
+                    return acc;
+                }, {} as Record<string, string>)
+            }
+        },
+        output: {
+            target: "web",
+            sourceMap
+        },
+        plugins: [
+            react && pluginReact(react({})),
+            svgr && pluginSvgr(svgr({
+                svgrOptions: {
+                    exportType: "named"
+                }
+            })),
+            ...plugins
+        ].filter(Boolean),
+        tools: {
+            rspack: {
+                infrastructureLogging: verbose ? {
+                    appendOnly: true,
+                    level: "verbose",
+                    debug: /PackFileCache/
+                } : undefined
+            }
+        }
+    };
+
+    const transformedConfig = applyTransformers(config, transformers, {
+        environment: "dev",
+        verbose
+    });
+
+    return defineConfig(transformedConfig);
+}
