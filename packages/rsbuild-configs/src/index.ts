73204eda439ba06f49f24db784f1834dade74b36
Patrick Lafrance <patricklafrance@users.noreply.github.com>
2024-12-15 17:17:30 -0500

    chore: Deploy Rsbuild configs 1.0.1 (#221)

--- packages/rsbuild-configs/src/index.ts
+++ packages/rsbuild-configs/src/index.ts
@@ -1,3 +1,4 @@
+export type { RsbuildConfigTransformer, RsbuildConfigTransformerContext } from "./applyTransformers.ts";
 export * from "./build.ts";
 export * from "./dev.ts";
 export * from "./storybook.ts";
